7ec660d7e9100974040e6c6c43d5187d50ac719f
Akshay Patel <patelaxay177@gmail.com>
2023-09-29 11:03:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import First from './components/first';
+// import First from './components/first';
+import { useEffect } from 'react';
+import browser from 'webextension-polyfill';
 
 function App() {
 
@@ -9,9 +11,31 @@ function App() {
     console.log('Value check :- ', value)
   }
 
+  useEffect(() => {
+
+    new Promise((resolve, reject) => {
+      if (typeof browser !== 'undefined' && typeof browser.runtime !== 'undefined') {
+        const action = 'GREETING';
+        const data = { name: 'John' };
+        console.log('First')
+        browser.runtime.sendMessage({ action, data })
+          .then(response => {
+            resolve(response.reply);
+          })
+          .catch(error => {
+            reject(error);
+          });
+        console.log('second')
+      } else {
+        reject("This code should only run in a browser extension environment.");
+      }
+    })
+
+  }, [])
+
   return (
     <div>
-      <First para={first} checkThis={checkFunction}></First>
+      {/* <First para={first} checkThis={checkFunction}></First> */}
     </div>
   );
 }
---
+++ src/Background/index.js
@@ -0,0 +1,6 @@
+import browser from 'webextension-polyfill';
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'GREETING') {
+        sendResponse({ reply: `Hello, ${message.data.name}!` });
+    }
+});
